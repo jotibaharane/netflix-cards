07202a44e433b3464ea33100214fcfd58a92a4e9
jotiba <87903172+jotibaharane@users.noreply.github.com>
2022-02-11 13:12:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ ReactDOM.render(
   <h1 className="heding_style" >List Of Top Five Netflix In 2022 </h1>
     {Sdata.map((val)=>{
        return(
-       <Cards imgsrc={val.imgsrc}
+       <Cards 
+       key={index}
+       imgsrc={val.imgsrc}
        title={val.title}
        source={val.source}
        sname={val.sname}
